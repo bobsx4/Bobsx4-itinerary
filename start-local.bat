1d7df287725416a5014be407475c04817a6b1bcc
bobsx4 <309215773+bobsx4@users.noreply.github.com>
2026-07-26 08:43:21 -0600

    prep for v0,2,0 load

--- start-local.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" cmd /c "timeout /t 2 >nul & start http://localhost:8765"
-where py >nul 2>nul
-if %errorlevel%==0 (
-  py -m http.server 8765
-) else (
-  python -m http.server 8765
-)
