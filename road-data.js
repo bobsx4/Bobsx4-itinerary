eb05ccd4bc66e447d3f3694210ec5000ff80c7ae
bobsx4 <r2kg749wbr@privaterelay.appleid.com>
2026-07-28 12:05:48 -0600

    fresh install - deleting existing

--- road-data.js
+++
@@ -1,8 +0,0 @@
-{
-  "app": "Bobsx4 Road Companion",
-  "version": "0.3.0 RC3.2",
-  "versionCode": "0.3.0-rc3.2",
-  "buildDate": "2026-07-28",
-  "dataSchema": 3,
-  "channel": "release-candidate"
-}
